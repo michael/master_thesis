866f3eeee97637c79b12ce059a25abf9a7e82b4a
Michael Aufreiter <ma@zive.at>
2011-11-22 13:56:51 +0100

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 pdflatex -halt-on-error -interaction nonstopmode -output-directory build index.tex
-bibtex build/index.aux
+cp literatur.bib build/literatur.bib
+cd build
+bibtex index.aux
+cd ..
 pdflatex -halt-on-error -interaction nonstopmode -output-directory build index.tex
 pdflatex -halt-on-error -interaction nonstopmode -output-directory build index.tex
